2635e453cd64c6f1d92a333b544a6e22768175f6
muhammadali246397 <muhammadali246397@gmail.com>
2023-10-27 23:43:17 +0600

    form password validation okay

--- src/component/landingPage/form/Form.jsx
+++ src/component/landingPage/form/Form.jsx
@@ -24,36 +24,20 @@ const Form = () => {
     };
     return (
         <div className='container mx-auto px-16'>
-
-            {/* <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label className="">
-    //             <span className="label-text text-base">Email</span>
-    //                   </label>
-                    <input placeholder='Email' className='border'
-                        {...register("firstName", { required: true })}
-                        aria-invalid={errors.firstName ? "true" : "false"}
-                    />
-                    {errors.firstName?.type === 'required' && <p role="alert">First name is required</p>}
-                </div>
-
-                <input
-                    {...register("mail", { required: "Email Address is required" })}
-                    aria-invalid={errors.mail ? "true" : "false"}
-                />
-                {errors.mail && <p role="alert">{errors.mail?.message}</p>}
-
-                <input type="submit" />
-            </form> */}
             <div className="">
                 <div className="card w-2/6">
                     <div className="card-body bg-slate-200 rounded-lg px-5 border">
+                        <h3 className='font-semibold text-3xl'>Resistration form</h3>
+                        <p>register your account here</p>
+
+                        <div className="divider">o</div>
+
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text text-base ">Email</span>
                                 </label>
-                                <input className="input input-bordered" placeholder='Email'
+                                <input className="input input-bordered" placeholder='Email' type='email'
                                     {...register("mail", { required: "Email Address is required" })}
                                     aria-invalid={errors.mail ? "true" : "false"}
                                 />
@@ -63,7 +47,7 @@ const Form = () => {
                                 <label className="label">
                                     <span className="label-text text-base ">UserName</span>
                                 </label>
-                                <input className="input input-bordered" placeholder='User Name'
+                                <input className="input input-bordered" type='text' placeholder='User Name'
                                     {...register("username", { required: "User name is required" })}
                                     aria-invalid={errors.username ? "true" : "false"}
                                 />
@@ -79,7 +63,7 @@ const Form = () => {
                                 />
                                 {errors.password && <p className='text-red-500' role="alert">{errors.password?.message}</p>}
                             </div>
-                           
+
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text text-base ">Confirm Password</span>
@@ -90,10 +74,11 @@ const Form = () => {
                                 />
                                 {errors.conpassword && <p className='text-red-500' role="alert">{errors.password?.message}</p>}
                             </div>
-                           
+
                             <div className='text-center mt-10'>
-                                <input className="btn bg-orange-700 px-6 py-2 border-none w-full hover:bg-orange-600 hover:text-black rounded-3xl" type="submit" value='Login' />
+                                <input className="btn bg-[#D80027] px-6 py-2 border-none w-full hover:bg-[rgb(255,149,168)] text-white rounded-sm" type="submit" value='Login' />
                             </div>
+                            <div className="divider">o</div>
                         </form>
                     </div>
                 </div>
