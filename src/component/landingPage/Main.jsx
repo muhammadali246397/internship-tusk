15cefd60990db56207c20d6dce0bbfa03997cc65
muhammadali246397 <muhammadali246397@gmail.com>
2023-10-28 09:52:47 +0600

    complete blog section

--- src/component/landingPage/Main.jsx
+++ src/component/landingPage/Main.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Form from './form/Form';
+import Blog from './blog/Blog';
 
 const Main = () => {
     return (
         <div>
-            <div className='my-20 '>
+            <div className='py-20 '>
                 <div className='container mx-auto px-16'>
-                    <button className='btn text-white bg-blue-600 px-6 py-3 mr-20 hover:bg-blue-800 font-semibold '>BOOK NOW</button>
-                    <button className='btn bg-gradient-to-r from-[#2FDFEC] py-3 px-6 via-[#45B0F4] via-30% via-[#6272FF] via-45% via-[#8959FD] via-60% via-[#B43DFB] via-70% to-[#F646E5] text-white hover:from-[#F646E5] hover:to-[#2FDFEC] font-semibold '>BOOK NOW</button>
+                    <button className='btn text-white bg-blue-600 px-6 border-none py-3 mr-20 hover:bg-blue-800 font-semibold '>BOOK NOW</button>
+                    <button className='btn bg-gradient-to-r from-[#2FDFEC] border-none py-3 px-6 via-[#45B0F4] via-30% via-[#6272FF] via-45% via-[#8959FD] via-60% via-[#B43DFB] via-70% to-[#F646E5] text-white hover:from-[#F646E5] hover:to-[#2FDFEC] font-semibold '>BOOK NOW</button>
                 </div>
             </div>
             <Form></Form>
+            <Blog></Blog>
         </div>
     );
 };
---
+++ src/component/landingPage/blog/Blog.jsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import banner from '../../../assets/banar.png'
+import user from '../../../assets/Image.png'
+import flag from '../../../assets/flag.png'
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { BiCommentDetail, BiLike, BiShare, BiShareAlt } from "react-icons/bi";
+
+
+const Blog = () => {
+    return (
+        <div>
+            <div className='container mx-auto px-16 mt-20'>
+                <div className='bg-white rounded-md p-5'>
+                    <h2 className='text-center font-bold text-4xl'><span className='border-b-2 border-red-500'>Blog</span></h2>
+                    <p className='text-center mt-2 mb-6'>Encuentra los artículos relacionados a tu sector y entérate de las ultimas noticias.</p>
+                    <div className='grid box-border  grid-cols-3 gap-6'>
+                        <div className='shadow-md hover:scale-105 transition-all'>
+                            <img className='transition-all' src={banner} alt="" />
+                            <div className='px-3 '>
+                                <div className='flex relative mt-5 gap-5'>
+                                    <img className='absolute  -top-[52px]' src={user} alt="" />
+                                    <div className='ml-20' >
+                                        <h2 className='text-1xl font-bold'>Cristian Fernández</h2>
+                                        <p className='font-semibold '>Derecho penal</p>
+                                        <div className='flex gap-1'>
+                                            <FaStar className='text-red-500'></FaStar>
+                                            <FaStar className='text-red-500'></FaStar>
+                                            <FaStarHalfAlt className='text-red-500'></FaStarHalfAlt>
+                                            <FaStar></FaStar>
+                                            <FaStar></FaStar>
+                                        </div>
+
+                                    </div>
+                                    <div>
+                                        <img className='ml-5 w-100%' src={flag} alt="" />
+                                    </div>
+                                </div>
+                                <h3 className='text-2xl font-bold my-3'>Title of blog post</h3>
+                                <p className='text-gray-600'>Etiam volutpat sem vitae erat consectetur tincidunt. Pellentesque mattis purus sed odio vehicula, vitae maximus nulla venenatis. Duis posuere turpis turpis, nec mollis nulla tincidunt sodales.</p>
+                                <div className='flex gap-3 my-4'>
+                                    <div className='border rounded-3xl px-3'>Pill labels</div>
+                                    <div className='border rounded-3xl px-3'>Pill labels</div>
+                                    <div className='border rounded-3xl px-3'>Pill labels</div>
+
+                                </div>
+                                <hr />
+                                <div className='flex justify-between my-2 py-4'>
+                                    <div className='flex gap-1 items-center'>
+                                        <BiCommentDetail></BiCommentDetail>
+                                        <p>123</p>
+                                    </div>
+                                    <div className='flex gap-1 items-center'>
+                                        <BiLike></BiLike>
+                                        <p>340</p>
+                                    </div>
+                                    <button className='btn btn-outline border-2 rounded-sm'>compiter <BiShareAlt></BiShareAlt></button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='shadow-md hover:scale-105 transition-all'>
+                            <img className=' transition-all' src={banner} alt="" />
+                            <div className='px-3 '>
+                                <div className='flex relative mt-5 gap-5'>
+                                    <img className='absolute  -top-[52px]' src={user} alt="" />
+                                    <div className='ml-20' >
+                                        <h2 className='text-1xl font-bold'>Cristian Fernández</h2>
+                                        <p className='font-semibold '>Derecho penal</p>
+                                        <div className='flex gap-1'>
+                                            <FaStar className='text-red-500'></FaStar>
+                                            <FaStar className='text-red-500'></FaStar>
+                                            <FaStarHalfAlt className='text-red-500'></FaStarHalfAlt>
+                                            <FaStar></FaStar>
+                                            <FaStar></FaStar>
+                                        </div>
+
+                                    </div>
+                                    <div>
+                                        <img className='ml-5 w-100%' src={flag} alt="" />
+                                    </div>
+                                </div>
+                                <h3 className='text-2xl font-bold my-3'>Title of blog post</h3>
+                                <p className='text-gray-600'>Etiam volutpat sem vitae erat consectetur tincidunt. Pellentesque mattis purus sed odio vehicula, vitae maximus nulla venenatis. Duis posuere turpis turpis, nec mollis nulla tincidunt sodales.</p>
+                                <div className='flex gap-3 my-4'>
+                                    <div className='border rounded-3xl px-3'>Pill labels</div>
+                                    <div className='border rounded-3xl px-3'>Pill labels</div>
+                                    <div className='border rounded-3xl px-3'>Pill labels</div>
+
+                                </div>
+                                <hr />
+                                <div className='flex justify-between my-2 py-4'>
+                                    <div className='flex gap-1 items-center'>
+                                        <BiCommentDetail></BiCommentDetail>
+                                        <p>123</p>
+                                    </div>
+                                    <div className='flex gap-1 items-center'>
+                                        <BiLike></BiLike>
+                                        <p>340</p>
+                                    </div>
+                                    <button className='btn btn-outline border-2 rounded-sm'>compiter <BiShareAlt></BiShareAlt></button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='shadow-md box-border hover:scale-105 transition-all'>
+                            <img className=' transition-all' src={banner} alt="" />
+                            <div className='px-3 '>
+                                <div className='flex relative mt-5 gap-5'>
+                                    <img className='absolute  -top-[52px]' src={user} alt="" />
+                                    <div className='ml-20' >
+                                        <h2 className='text-1xl font-bold'>Cristian Fernández</h2>
+                                        <p className='font-semibold '>Derecho penal</p>
+                                        <div className='flex gap-1'>
+                                            <FaStar className='text-red-500'></FaStar>
+                                            <FaStar className='text-red-500'></FaStar>
+                                            <FaStarHalfAlt className='text-red-500'></FaStarHalfAlt>
+                                            <FaStar></FaStar>
+                                            <FaStar></FaStar>
+                                        </div>
+
+                                    </div>
+                                    <div>
+                                        <img className='ml-5 w-100%' src={flag} alt="" />
+                                    </div>
+                                </div>
+                                <h3 className='text-2xl font-bold my-3'>Title of blog post</h3>
+                                <p className='text-gray-600'>Etiam volutpat sem vitae erat consectetur tincidunt. Pellentesque mattis purus sed odio vehicula, vitae maximus nulla venenatis. Duis posuere turpis turpis, nec mollis nulla tincidunt sodales.</p>
+                                <div className='flex gap-3 my-4'>
+                                    <div className='border rounded-3xl px-3'>Pill labels</div>
+                                    <div className='border rounded-3xl px-3'>Pill labels</div>
+                                    <div className='border rounded-3xl px-3'>Pill labels</div>
+
+                                </div>
+                                <hr />
+                                <div className='flex justify-between my-2 py-4'>
+                                    <div className='flex gap-1 items-center'>
+                                        <BiCommentDetail></BiCommentDetail>
+                                        <p>123</p>
+                                    </div>
+                                    <div className='flex gap-1 items-center'>
+                                        <BiLike></BiLike>
+                                        <p>340</p>
+                                    </div>
+                                    <button className='btn btn-outline border-2 rounded-sm'>compiter <BiShareAlt></BiShareAlt></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
