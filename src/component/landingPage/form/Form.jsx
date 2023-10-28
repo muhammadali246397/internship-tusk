c49cfd619d83fc9466ef413c4df723f9e72ab849
muhammadali246397 <muhammadali246397@gmail.com>
2023-10-28 07:44:03 +0600

    complete all form work

--- src/component/landingPage/form/Form.jsx
+++ src/component/landingPage/form/Form.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 
 const Form = () => {
@@ -7,21 +8,7 @@ const Form = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
     const onSubmit = (data) => console.log(data);
 
-    const validatePassword = (value) => {
-        if (!value) {
-            return 'Password is required';
-        }
-        if (value.length < 6) {
-            return 'Password must be at least 6 characters long';
-        }
-        if (!/(?=.*[A-Z])/.test(value)) {
-            return 'Password must contain at least one capital letter';
-        }
-        if (!/(?=.*[!@#$%^&*()])/.test(value)) {
-            return 'Password must contain at least one special character';
-        }
-        return true;
-    };
+ 
     return (
         <div className='container mx-auto px-16'>
             <div className="">
@@ -78,7 +65,13 @@ const Form = () => {
                             <div className='text-center mt-10'>
                                 <input className="btn bg-[#D80027] px-6 py-2 border-none w-full hover:bg-[rgb(255,149,168)] text-white rounded-sm" type="submit" value='Login' />
                             </div>
+                            <p className='text-red-500 text-center underline mt-5'> <Link >Forget yur password?</Link ></p>
+                           
+                            
                             <div className="divider">o</div>
+                            <p className='text-center'>Do not you have an account yet?</p>
+                            <p className='text-red-500 text-center underline mt-2'><Link>create one here</Link></p>
+
                         </form>
                     </div>
                 </div>
